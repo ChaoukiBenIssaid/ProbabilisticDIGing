91fb854df770e469414c1b97e281f9e7e8db80a2
gcaillet <gatien.caillet47@hotmail.com>
2021-06-27 18:27:41 +0300

    added DIG algorithm

--- Simulations/linear_regression.py
+++ Simulations/linear_regression.py
@@ -137,3 +137,65 @@ def sim_logistic_regression(X, y, n, iter_max, learning_rate, lmbd = 0, standard
         loss = np.mean([logistic_loss(thetas[i], X_test[i], y_test[i], lmbd) for i in range(n)])
         losses.append(loss)
     return losses
+
+
+
+def DIGing_linear_reg(X, y, n, iter_max, learning_rate, standardize = False, seed=None):
+    """Simulate a linear regression between n agents"""
+    rng = np.random.default_rng(seed)
+    nb_features = X.shape[-1] +1  #+1 for bias
+
+
+    X_split, y_split = random_split(X, y, n, seed = seed)
+    train_test_datasets = [train_test_split(X_split[i], y_split[i], random_state = seed) for i in range(n)]
+    X_train, X_test, y_train, y_test = [[tts_agent[i] for tts_agent in train_test_datasets] for i in range(4)]
+    if standardize:
+        scalers = [StandardScaler() for _ in range(n)]
+        for i in range(n) : 
+            X_train[i] = scalers[i].fit_transform(X_train[i]) #scale each agent's dataset independently
+            X_test[i] = scalers[i].transform(X_test[i]) 
+
+    X_train = [np.append(X_train[i], np.ones((X_train[i].shape[0],1)), axis = 1) for i in range(n)] #add a column of ones
+    X_test = [np.append(X_test[i], np.ones((X_test[i].shape[0],1)), axis = 1) for i in range(n)] #add a column of ones
+
+
+    thetas = [rng.standard_normal(nb_features) for _ in range(n)] 
+    deltas = [lr_grad(thetas[i], X_train[i], y_train[i]) for i in range(n)]
+
+
+    losses = list() 
+    communication_costs = [0]
+    for _ in range(iter_max):
+        #Dynamic graph
+        A = generate_random_adjacency_matrix(n, seed)
+        W = metropolis_weights(A)
+        #
+        new_thetas = thetas.copy()
+        new_deltas = deltas.copy()
+        
+        for i in range(n):
+            #theta update
+            Wtheta = [W[i,j]*thetas[j] for j in range(n)]
+            sum_Wtheta = np.sum(Wtheta, axis = 0)
+            new_thetas[i] = sum_Wtheta - learning_rate * new_deltas[i]
+
+            #delta update
+            Wdelta = [W[i,j]*deltas[j] for j in range(n)]
+            sum_Wdelta = np.sum(Wdelta, axis = 0)
+            grad = lr_grad(thetas[i], X_train[i], y_train[i])
+            new_grad = lr_grad(new_thetas[i], X_train[i], y_train[i])
+            new_deltas[i] = sum_Wdelta + new_grad - grad
+        thetas = new_thetas
+        deltas = new_deltas
+        loss = np.mean([lr_loss(thetas[i], X_test[i], y_test[i]) for i in range(n)])
+        losses.append(loss)
+    
+        #communication costs part 
+        nb_comms = np.sum(degrees(A))
+        communication_costs.append(2*nb_features * nb_comms + communication_costs[-1])
+
+
+    results = dict()
+    results["losses"] = losses
+    results["comm_costs"] = communication_costs[1:]
+    return results
